430b47fcaced760af4204cf31c454058b7e27bfc
cxx0505 <381142773@qq.com>
2024-04-21 14:27:04 +0800

    feat: 0421

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
+// 1、父组件传递数据 子组件标签身上绑定属性
+// 2、子组件接受数据 props的参数
+// props：对象里包含了父组件传过来的所有的数据
+// {name:'父组件中的数据'}
+function Son(props) {
+  console.log(props);
+  return <div>this is son,{props.name}</div>
+}
 function App() {
+  const name = 'this is app name'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Son name = {name} />
     </div>
-  );
+  )
 }
 
 export default App;
